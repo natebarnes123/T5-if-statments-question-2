5a81543c3f89673cc0549b9a49557644a9a51af3
Nathan Barnes <339723215@GoTVDSB.ca>
2024-10-03 08:33:28 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace T5_if_statments_question_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
